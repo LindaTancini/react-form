90b95e77990fc952ff84cab5687ea92863132c81
LindaTancini <lindatancini95@gmail.com>
2025-04-09 13:51:26 +0200

    feat: create html for articles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ function App() {
   // LISTA DEGLI ARTICOLI
   const [articles, setArticles] = useState("Manga");
   console.log(articles);
+  return (
+    <>
+      <div>
+        <h1>Lista di Articoli </h1>
+        <ul>
+          <li>{articles}</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
