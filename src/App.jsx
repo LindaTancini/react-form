28858ecdbd50cfe8d0629e6e91bbdfdcb3b6064d
LindaTancini <lindatancini95@gmail.com>
2025-04-09 14:32:29 +0200

    feat: added form

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 
 function App() {
   // LISTA DEGLI ARTICOLI
-  const [articles, setArticles] = useState("Manga");
+  const [articles, setArticles] = useState(["Manga"]);
+  // AGGIUNGO UN NUOVO ARTICOLO
+  const [newArticles, setNewArticles] = useState("");
   console.log(articles);
+
+  // AGGIUNGO PREVENT DEFAULT PER NON FAR RICARICARE LA PAGINA
+  const addNewArticles = (event) => {
+    event.preventDefault();
+    //CON PUSH AGGIUNGO I NUOVI ARTICOLI
+    articles.push(newArticles);
+    console.log(newArticles);
+  };
+
   return (
     <>
       <div>
@@ -11,6 +22,16 @@ function App() {
         <ul>
           <li>{articles}</li>
         </ul>
+        {/*CONTROLLO L'EVENTO DI INVIO DEL FORM*/}
+        <form onSubmit={addNewArticles}>
+          <input
+            type="text"
+            value={newArticles}
+            //PRENDIAMO IL VALORE E LO INSERIAMO
+            onChange={(event) => setNewArticles(event.target.value)}
+          />
+          <button>Aggiungi Articolo</button>
+        </form>
       </div>
     </>
   );
